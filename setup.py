d4e25810440486d5168501602e1eb1834ff1591f
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-04-07 00:57:45 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="prelude-django-admin-uikit",
+    version="0.1.0",
+    packages=['prelude_django_admin_uikit'],
+
+    install_requires=[
+        'django>=3'
+    ],
+
+    include_package_data=True,
+
+    author="Jonhnatha Trigueiro",
+    author_email="joepreludian@gmail.com",
+    description="Template for Django admin based on uikit",
+
+    keywords="django admin uikit",
+    url="https://github.com/joepreludian/prelude-django-admin-toolkit",
+    project_urls={
+        "Bug Tracker": "https://github.com/joepreludian/prelude-django-admin-toolkit/issues/new",
+        "Documentation": "https://github.com/joepreludian/prelude-django-admin-toolkit",
+        "Source Code": "https://github.com/joepreludian/prelude-django-admin-toolkit",
+    },
+
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ]
+)
