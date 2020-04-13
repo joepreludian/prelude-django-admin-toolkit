4569918a8d079680d9c7fa4c162f5f993f2c7990
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-04-13 01:08:41 -0300

    Update more admin pages

--- prelude_django_admin_uikit/admin.py
+++ prelude_django_admin_uikit/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = 'Prl Admin'
+admin.site.site_title = 'Prelude Brunah'
+admin.site.index_title = f'Welcome!'
