3a173da6344faa114bf3b3b95b56ef6ca99337cf
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-10-11 01:13:56 -0300

    Update Splinter

--- testproject/features/environment.py
+++ testproject/features/environment.py
@@ -2,7 +2,7 @@ from splinter import Browser
 
 
 def before_all(context):
-    context.browser = Browser()
+    context.browser = Browser('firefox')
     context.server_url = 'http://localhost:8000'
 
 def after_all(context):
