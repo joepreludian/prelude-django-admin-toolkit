857daa61bb21ad3b4a1b3b19bb0484dc2f93d515
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-10-11 01:14:47 -0300

    Update Splinter in order to use Headless mode

--- testproject/features/environment.py
+++ testproject/features/environment.py
@@ -2,7 +2,7 @@ from splinter import Browser
 
 
 def before_all(context):
-    context.browser = Browser('firefox')
+    context.browser = Browser('firefox', headless=True)
     context.server_url = 'http://localhost:8000'
 
 def after_all(context):
